b4a358014a2c72b589b747ea58975b730e39eb0f
Harald Wartig <hwartig@gmail.com>
2023-06-23 23:07:51 +0200

    Add debugging

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-awk -v FS='\t' -v OFS='' '($4=="statuspage") {print "mkdir -p data/",$1,"; curl -s -f ",$3,"/api/v2/summary.json | jq > data/",$1,"/summary.json"}' companies.tsv | bash
-awk -v FS='\t' -v OFS='' '($4=="statuspage") {print "mkdir -p data/",$1,"; curl -s -f ",$3,"/api/v2/incidents.json | jq > data/",$1,"/incidents.json"}' companies.tsv | bash
-awk -v FS='\t' -v OFS='' '($4=="instatus") {print "mkdir -p data/",$1,"; curl -s -f ",$3," | hxnormalize -x | hxselect -c \"#__NEXT_DATA__\" | jq \".props.pageProps | del(.startDate,.endDate)\" > data/",$1,"/data.json"}' companies.tsv | bash
+echo "Fetching statuspage"
+awk -v FS='\t' -v OFS='' '($4=="statuspage") {print "echo ",$1,"; mkdir -p data/",$1,"; curl -s -f ",$3,"/api/v2/summary.json | jq > data/",$1,"/summary.json"}' companies.tsv | bash
+awk -v FS='\t' -v OFS='' '($4=="statuspage") {print "echo ",$1,"; mkdir -p data/",$1,"; curl -s -f ",$3,"/api/v2/incidents.json | jq > data/",$1,"/incidents.json"}' companies.tsv | bash
+echo "Fetching instatus"
+awk -v FS='\t' -v OFS='' '($4=="instatus") {print "echo ",$1,"; mkdir -p data/",$1,"; curl -s -f ",$3," | hxnormalize -x | hxselect -c \"#__NEXT_DATA__\" | jq \".props.pageProps | del(.startDate,.endDate)\" > data/",$1,"/data.json"}' companies.tsv | bash
